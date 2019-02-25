9f8e962547131b19cd76797302547dca4a697d0c
Daniel Schneider <daschne8@gmail.com>
2019-02-25 17:27:18 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     return array.join(" and ")
   when 3
     return array[0,2].join(", ") + ", and " + array[2]
-  when > 3
+  else
     len = array.length
     return array[0,len-1].join(", ") + ", and " + array[len-1]
   end
